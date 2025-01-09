e654761d463769d22ea8512928b26c3bda057ee9
BD103 <59022059+BD103@users.noreply.github.com>
2025-01-09 11:14:32 -0500

    feat: register `bevy` as a default tool

--- bevy_lint/src/callback.rs
+++ bevy_lint/src/callback.rs
@@ -1,5 +1,18 @@
+use std::sync::Once;
+
 use rustc_driver::Callbacks;
 use rustc_interface::interface::Config;
+use rustc_lint_defs::RegisteredTools;
+use rustc_middle::ty::TyCtxt;
+use rustc_span::symbol::Ident;
+
+/// The original `registered_tools()` query.
+///
+/// Because we overwrite `registered_tools()` with our own version, we save the original in this
+/// static so it can be called later.
+static mut REGISTERED_TOOLS: fn(TyCtxt<'_>, ()) -> RegisteredTools = |_, _| {
+    unreachable!("This function will be overwritten when `BevyLintCallback::config()` is run.")
+};
 
 /// The `rustc` [`Callbacks`] that register Bevy's lints.
 pub struct BevyLintCallback;
@@ -24,5 +37,36 @@ impl Callbacks for BevyLintCallback {
             crate::lints::register_passes(store);
             crate::groups::register_groups(store);
         }));
+
+        debug_assert!(
+            config.override_queries.is_none(),
+            "`override_queries()` already exists, but it would be replaced.",
+        );
+
+        config.override_queries = Some(|_session, providers| {
+            static INIT: Once = Once::new();
+
+            // SAFETY: `REGISTERED_TOOLS` is only written to here, and is not read by our custom
+            // `registered_tools()` until later in the program.
+            INIT.call_once(|| unsafe {
+                // Save the original `registered_tools()` query so that our new version can still
+                // call it.
+                REGISTERED_TOOLS = providers.queries.registered_tools;
+            });
+
+            // Overwrite the `registered_tools()` query with our own version.
+            providers.queries.registered_tools = registered_tools;
+        });
     }
+}
+
+/// A version of the `registered_tools()` compiler query that also includes `bevy` by default.
+fn registered_tools(tcx: TyCtxt<'_>, _: ()) -> RegisteredTools {
+    // SAFETY: `REGISTERED_TOOLS` is not modified after it is first set in
+    // `BevyLintCallback::config()`. Queries are not run until after that function finishes.
+    let mut tools = unsafe { REGISTERED_TOOLS(tcx, ()) };
+
+    tools.insert(Ident::from_str("bevy"));
+
+    tools
 }
